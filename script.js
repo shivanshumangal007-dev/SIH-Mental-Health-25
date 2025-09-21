23670c3b18c369e42b2aba9f91b2ebfddae156e3
shivanshu <shivanshumangal007@gmail.com>
2025-09-21 13:32:16 +0530

    reponsive + severe help btn

---
+++ script.js
@@ -0,0 +1,9 @@
+function hideSidebar(){
+    let cross = document.querySelector(".sidebar");
+
+    cross.classList.toggle("active");
+}
+function showSidebar() {
+  let cross = document.querySelector(".sidebar");
+  cross.classList.toggle("active");
+}
